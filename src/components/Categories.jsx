3521403df9ceddcd54b34349bef4c50b42e00871
Alone107 <AloneOmen@yandex.ru>
2024-02-17 22:03:41 +0500

    Add All

---
+++ src/components/Categories.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+export const Categories = () => {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
+  const onClickCategories = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, i) => (
+          <li
+            key={i}
+            onClick={() => onClickCategories(i)}
+            className={activeIndex == i ? "active" : ""}
+          >
+            {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
